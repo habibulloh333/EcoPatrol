3e5f578fc45632b4d153d9d357f7b5b54fc8de62
zeinurrazikin <mohamadzeinurrazikin@gmail.com>
2025-12-09 16:52:05 +0700

    mahasiswa 1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Plugin Google Services DIPINDAHKAN KE SINI (Hanya boleh 1 blok plugins di paling atas)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,10 +22,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // Ganti applicationId jika perlu, pastikan sama dengan yang terdaftar di Firebase
         applicationId = "com.example.ecopatrol"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +32,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -43,25 +41,8 @@ flutter {
     source = "../.."
 }
 
-plugins {
-    id("com.android.application")
-
-    // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
-
-    ...
-}
-
 dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
-
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
-
-
-    // Add the dependencies for any other desired Firebase products
-    // https://firebase.google.com/docs/android/setup#available-libraries
 }
